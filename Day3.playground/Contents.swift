30dc0f08657343df63f2a7a7efac5c80284e86f0
Andreas Liebich <aliebich@lcs.on.ca>
2020-10-01 09:51:58 -0400

    Day three playground notes

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -10,3 +10,45 @@ Please [commit your work frequently](https://www.russellgordon.ca/cs/source-cont
 import Cocoa
 
 var str = "Hello, playground"
+
+let firstscore = 7
+let secondscore = 8
+let total = firstscore + secondscore
+let difference = secondscore - firstscore
+let product = firstscore * secondscore
+let divided = firstscore / secondscore
+let remainder = 13 % secondscore
+
+let meaningOfLife = 42
+let doublemeaning = 42 + 42
+let fakers = "fakers gonna"
+let action + "fake"
+let firsthalf = ["john", "Paul"]
+let secondhal = ["george", "Ringo"]
+let beatles = firsthalf + secondhalf
+
+var score = 95
+score -= 5
+var quote = "The rain in sapin falls mainly on the"
+quote += "spainiards"
+
+let firstscore = 6
+let secondscore = 4
+firtscore == secondscore //this checks if two scores are equal if (!) pops up they are nopt equal
+firstscore != secondscore
+firstscore < secondscore
+firstscore >= secondscore
+"Taylor" <= "Swift"
+
+let firstcard = 11
+let secondcard = 1-
+
+if first card + second card == 21 {
+    print("blackjack!")
+} else {
+    print("Regular cards")
+    
+}
+
+
+
